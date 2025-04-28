266e18c696813aa52f52f000642543933e3a94bf
Hashim326 <71702199+Hashim326@users.noreply.github.com>
2025-04-28 14:52:47 +0100

    Created task services

---
+++ Frontend/TaskManagerFrontend/src/app/services/task.service.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl = ' https://localhost:7101/api/auth';
+
+  constructor(private http: HttpClient) {}
+
+  public getAllTasks() : Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}`);
+  }
+
+  public getTaskById(taskId: number) : Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${taskId}`);
+  }
+  
+  public createTask(title: string, description: string, status: number, dueDate: Date ) : Observable<any>{
+    const body = {
+      title, description, status, dueDate
+    }
+    return this.http.post<any>(`${this.apiUrl}`, body);
+  }
+
+  public updateTask(taskId: number, title: string, description: string, status: number, dueDate: Date ) : Observable<any>{
+    const body = {
+      title, description, status, dueDate
+    }
+    return this.http.put<any>(`${this.apiUrl}/${taskId}`, body);
+  }
+
+  public deleteTask(taskId: number) : Observable<any>{
+    return this.http.delete<any>(`${this.apiUrl}/${taskId}`);
+  }
+}
